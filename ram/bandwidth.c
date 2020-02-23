05f97b47a8d3cb506297535015455bebc5a961b1
Xuhao Luo <rossihox@gmail.com>
2020-02-22 16:34:20 -0800

    add bandwidth results.

--- ram/bandwidth.c
+++ ram/bandwidth.c
@@ -1,14 +1,15 @@
 #include "../include/measure.h"
+#include "../include/analysis.h"
 #include <stdlib.h>
 #include <string.h>
 
 #define NUM_OF_EXPERIMENTS 10
-#define LOOPS 1000
-#define MB 1024 * 1024
+#define KB 1024 
+#define MB 1024 * KB
 #define GB 1024 * MB
 
 void read_bandwidth() {
-    const size_t src_size = 128 * MB;
+    const size_t src_size = 1 * GB;
     const size_t des_size = 4 * MB;
     char* src = (char*) malloc(src_size);   // 100mb data should be sufficient to defeat any cache
     char* des = (char*) malloc(des_size);   // 4mb array can fit into cache, which reduce the writing overhead
@@ -17,26 +18,25 @@ void read_bandwidth() {
         exit(1);
     }
 
-    // initialize the src array
-    for (int i = 0; i < src_size; i++) {
-        src[i] = (char) rand();
-    }
-
+    double results[NUM_OF_EXPERIMENTS];
     for (int i = 0; i < NUM_OF_EXPERIMENTS; i++) {
         MEASURE_START();
 
         #pragma unroll
-        for (int j = 0; j < LOOPS; j++) {
-            int offset = rand() % (src_size / des_size) * des_size;
-            memcpy(des, (const void *)&src[offset], des_size);
+        for (int j = 0; j < src_size / des_size; j++) {
+            memcpy(des, (const void *)&src[j * des_size], des_size);
         }
 
         MEASURE_END();
 
         double seconds = (double) measure_time() / (2.4 * GB);   // cpu freq is 2.4GHz
-        double read_bandwidth = (double)4 * LOOPS / seconds;
-        printf("reading bandwidth: %lfMB/s\n", read_bandwidth);
+        results[i] = (double)1 / seconds;
+        printf("reading bandwidth: %lfGB/s\n", results[i]);
     }
+
+    double mean = getMean(results, NUM_OF_EXPERIMENTS);
+    double stddev = getStdDev(results, NUM_OF_EXPERIMENTS);
+    printf("mean: %lf, stddev: %lf\n", mean, stddev);
 
     free(src);
     free(des);
@@ -44,7 +44,7 @@ void read_bandwidth() {
 
 void write_bandwidth() {
     const size_t src_size = 4 * MB;
-    const size_t des_size = 128 * MB;
+    const size_t des_size = 1 * GB;
     char* src = (char*) malloc(src_size);
     char* des = (char*) malloc(des_size);
     if (src == NULL || des == NULL) {
@@ -52,28 +52,33 @@ void write_bandwidth() {
         exit(1);
     }
 
-    for (int i = 0; i < src_size; i++) {
-        src[i] = (char) rand();
-    }
-
+    double results[NUM_OF_EXPERIMENTS];
     for (int i = 0; i < NUM_OF_EXPERIMENTS; i++) {
         MEASURE_START();
 
         #pragma unroll
-        for (int j = 0; j < LOOPS; j++) {
-            int offset = rand() % (des_size / src_size) * src_size;
-            memcpy((void *)&des[offset], src, src_size);
+        for (int j = 0; j < des_size / src_size; j++) {
+            memcpy((void *)&des[j * src_size], src, src_size);
         }
 
         MEASURE_END();
 
         double seconds = (double) measure_time() / (2.4 * GB);
-        double write_bandwidth = (double)4 * LOOPS / seconds;
-        printf("writing bandwidth: %lfMB/s\n", write_bandwidth);
+        double write_bandwidth = (double)1 / seconds;
+        printf("writing bandwidth: %lfGB/s\n", write_bandwidth);
+        results[i] = write_bandwidth;
     }
+
+    double mean = getMean(results, NUM_OF_EXPERIMENTS);
+    double stddev = getStdDev(results, NUM_OF_EXPERIMENTS);
+    printf("mean: %lf, stddev: %lf\n", mean, stddev);
+
+    free(src);
+    free(des);
 }
 
 int main() {
+    set_affinity();
     read_bandwidth();
     write_bandwidth();
 }
